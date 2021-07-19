8e30a3d1d5e37fd76be837afa75af39cb9cf88b6
sajeer <sajeer850@gmail.com>
2021-07-19 06:05:44 +0400

    test

---
+++ install.sh
@@ -0,0 +1,15 @@
+###https://vincentteyssier.medium.com/install-magento-2-3-2-3f0a5b2945c4
+
+sudo apt update
+
+sudo apt install nginx -y
+
+sudo ufw app list
+
+curl -4 icanhazip.com
+
+sudo apt install mysql-server -y
+
+sudo mysql_secure_installation
+
+sudo apt install php-fpm php-mysql
